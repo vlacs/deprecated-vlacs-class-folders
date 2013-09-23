c5381aef141142f4d6650b4b9c2e6f813b69c256
Mike George <mikeyg6754@gmail.com>
2013-09-23 10:26:20 -0400

    Update view.sql

--- testing/view.sql
+++ testing/view.sql
@@ -1,11 +1,12 @@
 SELECT mmc.master_course_idstr as master_id,
-       mc.classroom_idstr as class_id,
+       mce.classroom_idstr as class_id,
        mmc.name as course_name,
-       mc.sis_user_idstr as teacher_id,
-       msu.firstname as teacher_firstname,
-       msu.lastname as teacher_lastname
+       mce.sis_user_idstr as student_id,
+       msu.firstname as student_firstname,
+       msu.lastname as student_lastname
        FROM
-           mdl_classroom mc LEFT OUTER JOIN mdl_master_course mmc on (mmc.master_course_idstr = mc.master_course_idstr)
-               LEFT OUTER JOIN mdl_sis_user msu on (msu.sis_user_idstr = mc.sis_user_idstr)
-       WHERE mc.status = 'ACTIVE'
+           mdl_sis_user msu LEFT OUTER JOIN mdl_classroom_enrolment mce on (mce.sis_user_idstr = msu.sis_user_idstr)
+       	LEFT OUTER JOIN mdl_classroom mc on (mc.classroom_idstr = mce.classroom_idstr)
+            	    LEFT OUTER JOIN mdl_master_course mmc on (mmc.master_course_idstr = mc.master_course_idstr)
+       WHERE msu.privilege = 'STUDENT' AND mc.status = 'ACTIVE' AND mce.status_idstr = 'ACTIVE'
        LIMIT(20);
