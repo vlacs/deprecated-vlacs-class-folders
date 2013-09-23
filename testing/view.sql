8993f85334a0cd0920208d487254542a832438b9
Mike George <mikeyg6754@gmail.com>
2013-09-23 11:06:46 -0400

    Update view.sql

--- testing/view.sql
+++ testing/view.sql
@@ -8,9 +8,9 @@ SELECT mmc.master_course_idstr as master_id,
        tsu.firstname as teacher_firstname,
        tsu.lastname as teacher_lastname
        FROM
-           mdl_sis_user msu LEFT JOIN mdl_classroom_enrolment mce on (mce.sis_user_idstr = msu.sis_user_idstr)
-               LEFT JOIN mdl_classroom mc on (mc.classroom_idstr = mce.classroom_idstr)
-                   LEFT JOIN mdl_master_course mmc on (mmc.master_course_idstr = mc.master_course_idstr)
-                       LEFT JOIN mdl_sis_user tsu on (mc.sis_user_idstr = tsu.sis_user_idstr)
+           mdl_classroom_enrolment mce JOIN mdl_sis_user msu on (msu.sis_user_idstr = mce.sis_user_idstr)
+           JOIN mdl_classroom mc on (mc.classroom_idstr = mce.classroom_idstr)
+           JOIN mdl_master_course mmc on (mmc.master_course_idstr = mc.master_course_idstr)
+           LEFT JOIN mdl_sis_user tsu on (mc.sis_user_idstr = tsu.sis_user_idstr)
        WHERE msu.privilege = 'STUDENT' AND mc.status = 'ACTIVE' AND mce.status_idstr = 'ACTIVE'
        ORDER BY mmc.master_course_idstr, mc.classroom_idstr;
