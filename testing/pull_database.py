2d64dc6aff3e082950e16882c2c456948fac17a8
Mike George <mikeyg6754@gmail.com>
2013-09-26 14:11:16 -0400

    Update pull_database.py

--- testing/pull_database.py
+++ testing/pull_database.py
@@ -28,7 +28,7 @@ def print_row(row):
 	print("--------------------------------------------------------------------------------------")
 	print("%sStudent Name: %s%-30s%sStudent Email: %s%-30s" % (GREEN, WHITE, row['student_firstname'] + " " + row['student_lastname'], GREEN, WHITE, row['student_email']))
 	print("%sClass:        %s%-30s%sClass ID:      %s%-30s" % (GREEN, WHITE, row['course_name'], GREEN, WHITE, row['class_id'])) 
-	print("%sTeacher Name: %s%-30s%sTeacher Email: %s%-30s" % (GREEN, WHITE, row['teacher_lastname'], GREEN, WHITE, row['teacher_email']))
+	print("%sTeacher Name: %s%-30s%sTeacher Email: %s%-30s" % (GREEN, WHITE, row['teacher_firstname'] + " " + row['teacher_lastname'], GREEN, WHITE, row['teacher_email']))
 
 if __name__ == "__main__":
 	main(sys.argv[1])
