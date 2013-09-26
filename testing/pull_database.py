af8cf6327c6f8591c53871814a3bcc956f620985
Mike George <mikeyg6754@gmail.com>
2013-09-26 10:29:05 -0400

    Closed DB connection incorrecly

--- testing/pull_database.py
+++ testing/pull_database.py
@@ -11,11 +11,12 @@ def main():
 							password=config.DATABASE['password'], database=config.DATABASE['database'])
 	cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 	cursor.execute("SELECT * FROM view_vlacs_class_folders LIMIT(20)")
-	conn.close()
 
 	for row in cursor:
 		print("First Name: %s Last Name: %s Class: %s", row['student_first_name'], 
 							row['student_last_name'], row['course_name'])
+
+	conn.close()
 
 if __name__ == "__main__":
 	main()
