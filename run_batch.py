1ece969b127cb021d4745360def788b54757238a
Mike George <mikeyg6754@gmail.com>
2013-09-29 10:36:15 -0400

    Fix error

--- run_batch.py
+++ run_batch.py
@@ -16,9 +16,8 @@ def main():
 		print("Processing row %s..." % (count))
 		cdb_query = "SELECT class_id, folder_id FROM vlacs_class_folders_structure WHERE class_id = %s;" % row['class_id']
 		check_db, conn_cdb = Database.get(query=cdb_query)
-		print cdb_query[0]
-		res = check_db.fetchone()
-		print res
+		print cdb_query
+		print res[0]z
 		#if len(list(check_db[0])) < 1:
 		#	classfolder_id = Database.get(query="SELECT folder_id FROM vlacs_class_folders_structure WHERE folder_name = 'VLACS Class Folders'")
 		#	archive_id = Database.get(query="SELECT folder_id FROM vlacs_class_folders_structure WHERE folder_name = 'VLACS Archive'")
