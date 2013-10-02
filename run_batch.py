ce26ab0306dce53dceac98a66c90d144907afe65
Mike George <mikeyg6754@gmail.com>
2013-10-02 08:12:43 -0400

    Update run_batch.py

--- run_batch.py
+++ run_batch.py
@@ -7,7 +7,7 @@ from Classes import Client
 from Classes import Folder
 
 def main():
-	result, conn = Database.get(limit=30)
+	result, conn = Database.get(limit=2)
 	client = Client.create()
 
 	count = 1
@@ -17,6 +17,8 @@ def main():
 		cdb_query = "SELECT class_id, folder_id FROM vlacs_class_folders_structure WHERE class_id = %s;" % row['class_id']
 		check_db, conn_cdb = Database.get(query=cdb_query)
 		if len(list(check_db)) < 1:
+			print "Folder not found, creating..."
+			print cdb_query
 			classfolder_id_db, conn_cf = Database.get(query="SELECT folder_id FROM vlacs_class_folders_structure WHERE folder_name = 'VLACS Class Folders';")
 			classfolder_id = classfolder_id_db.fetchone()
 
@@ -24,6 +26,7 @@ def main():
 			Folder.create(client, row['student_lastname'] + ", " + row['student_firstname'] + " - Assignments", classfolder.resource_id.text)
 			Database.close(conn_cf, classfolder_id_db)
 		else:
+			print "Folder Found..."
 			res = check_db.fetchone()
 			Folder.create(client, row['student_lastname'] + ", " + row['student_firstname'] + " - Assignments", res['folder_id'])
 		count += 1
