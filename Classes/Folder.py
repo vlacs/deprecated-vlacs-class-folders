5614c7f5358a777d9fa4a28c104f9e476799bc80
Mike George <mikeyg6754@gmail.com>
2013-10-02 11:00:04 -0400

    fix error

--- Classes/Folder.py
+++ Classes/Folder.py
@@ -17,7 +17,7 @@ def create(client, title, parent=None, class_id=None):
     
     #On success insert into database
     if parent != None and class_id != None:
-        Database.execute("INSERT INTO vlacs_class_folders_structure (class_id, folder_name, folder_id, folder_parent) VALUES ('%s', '%s', '%s');" % (class_id,title, folder.resource_id.text, parent.resource_id.text))
+        Database.execute("INSERT INTO vlacs_class_folders_structure (class_id, folder_name, folder_id, folder_parent) VALUES ('%s', '%s', '%s', '%s');" % (class_id,title, folder.resource_id.text, parent.resource_id.text))
     elif parent != None:
         Database.execute("INSERT INTO vlacs_class_folders_structure (folder_name, folder_id, folder_parent) VALUES ('%s', '%s', '%s');" % (title, folder.resource_id.text, parent.resource_id.text))
     else:
