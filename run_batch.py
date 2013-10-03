1dc870296700174f18f14135eeb19ff4b3b0608c
Mike George <mikeyg6754@gmail.com>
2013-10-03 13:26:03 -0400

    Implement proposed changes

--- run_batch.py
+++ run_batch.py
@@ -31,6 +31,7 @@ def main(limit=None, offset=None):
     for enrollment in enrollments:
         print("Processing enrollment %s/%s..." % (count, last_disp))
         folder_exists = Database.get(Database.execute(conn, Database.folder_exists_query_string(enrollment['class_id'])))
+        print "DEBUG (DIRECT PRINT): %s" % Database.get(Database.execute(conn, Database.folder_exists_query_string(enrollment['class_id'])))
         if folder_exists:
             print "DEBUG: folder_exists = %s" % folder_exists
             print "Class Folder Found..."
