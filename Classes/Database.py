036e367a75457017d0e157ca480a319762e47b12
Matt Oquist <moquist@majen.net>
2013-10-03 10:57:02 -0400

    break code and add comments in interactive, instructive, critically constructive code review

--- Classes/Database.py
+++ Classes/Database.py
@@ -14,6 +14,8 @@ def connect():
     return conn
 
 def execute(query):
+    # TODO
+    print "deleteme! (Database.execute)"
     conn = connect()
     cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
     cursor.execute(query)
@@ -21,8 +23,15 @@ def execute(query):
     cursor.close()
     conn.close()
 
-def get(query=None, limit=None, offset=None):
-    conn = connect()
+# TODO: separate the SQL generation (or selection, really) from get, so it can be called like this:
+# get(conn, Database.query_string(query=None, limit=None, offset=None))
+# data = Database.get(Database.execute(conn, Database.query_string(query=None, limit=None, offset=None)))
+# ...where query_string returns the SQL that will be used for the query,
+# ...and execute does the query and returns the cursor
+# ...and get does fetchall(), closes the cursor, and returns the data
+# ...maybe also have get1(), which takes the first thing returned by fetchall() (assuming that's always a list)
+# ...maybe also have ensure1(), which ensures that the list from fetchall() has length 1, and then calls get1(), else returns False
+def get(conn, query=None, limit=None, offset=None):
     cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
     if query == None:
         if limit == None and offset == None:
@@ -43,7 +52,7 @@ def get(query=None, limit=None, offset=None):
         else:
             cursor.execute("%s LIMIT(%s) OFFSET(%s);" % (query, limit, offset))
 
-    return cursor, conn
+    return cursor
 
 def close(connection, cursor=None):
     connection.close()
--- run_batch.py
+++ run_batch.py
@@ -8,53 +8,48 @@ from Classes import Client
 from Classes import Folder
 from Classes import Utilities
 
-def main(limit_in=None, offset_in=None):
-    result, conn = Database.get(limit=limit_in, offset=offset_in)
+def main(limit=None, offset=None):
+    conn = Database.connect()
     client = Client.create()
 
-    if offset_in != None:
-        offset = int(offset_in)
+    count = 1
+    if offset != None:
+        offset = int(offset)
         count = offset
-    else:
-        offset = None
-        count = 1
 
-    if limit_in != None:
-        limit = int(limit_in)
-    else:
-        limit = None
+    last_disp = "-"
+    if limit != None:
+        limit = int(limit)
+        last_disp = limit
 
+    if limit != None and offset != None:
+        last_disp = offset + limit
 
-    for row in result:
-        if limit != None:
-            if offset != None:
-                last = int(offset_in) + int(limit_in)
-                print("Processing row %s/%s..." % (count, last))
-            else:
-                print("Processing row %s/%s..." % (count, limit))
-        else:
-            print("Processing row %s..." % (count))
-        cdb_query = "SELECT class_id, folder_id FROM vlacs_class_folders_structure WHERE class_id = %s;" % row['class_id']
-        check_db, conn_cdb = Database.get(query=cdb_query)
-        if len(list(check_db)) < 1:
-            print "Class Folder not found, creating..."
-            rootclassfolder_id_db, conn_rcf = Database.get(query="SELECT folder_id FROM vlacs_class_folders_structure WHERE folder_name = 'VLACS Class Folders';")
-            rootclassfolder_id = rootclassfolder_id_db.fetchone()
-
-            classfolder = Folder.create(client, row['course_name'] + " - " + Utilities.clean_name(row['teacher_firstname']) + " " + Utilities.clean_name(row['teacher_lastname']) + " - " + Utilities.course_version(row['course_full_name']) + " - " + row['class_id'], rootclassfolder_id['folder_id'], row['class_id'])
-            Folder.create(client, Utilities.clean_name(row['student_lastname']) + ", " + Utilities.clean_name(row['student_firstname']) + " - Assignments", classfolder.resource_id.text)
-            Database.close(conn_rcf, rootclassfolder_id_db)
-        else:
+    enrollments = Database.get(Database.execute(conn, Database.query_string(limit=limit, offset=offset)))
+    for enrollment in enrollments:
+        print("Processing enrollment %s/%s..." % (count, last_disp))
+        cdb_query = "SELECT class_id, folder_id FROM vlacs_class_folders_structure WHERE class_id = %s;" % enrollment['class_id']
+        folder_exists = Database.ensure1(Database.execute(conn, Database.query_string(query=cdb_query)))
+        # TODO: check whether list(cur) consumes what the cursor contains
+        if folder_exists:
             print "Class Folder Found..."
-            #For some reason the script doesn't work without this redundant database call...
-            classfolder_id_db, conn_cf = Database.get(query=cdb_query)
-            classfolder_id = classfolder_id_db.fetchone()
-            Folder.create(client, Utilities.clean_name(row['student_lastname']) + ", " + Utilities.clean_name(row['student_firstname']) + " - Assignments", classfolder_id['folder_id'])
-            Database.close(conn_cf, classfolder_id_db)
-        count += 1
-    Database.close(conn_cdb, check_db)
-    Database.close(conn, result)
+            Folder.create(client, Utilities.clean_name(enrollment['student_lastname']) + ", " + Utilities.clean_name(enrollment['student_firstname']) + " - Assignments", folder_exists['folder_id'])
+        else:
+            # TODO: get folder title here (see Utilities.title() below :), and display it
+            print "Class Folder not found, creating: %s" % title
+            # TODO: make "VLACS Class Folders" a configurable setting
+            # TODO: remove semicolon if it's unnecessary?
+            rootclassfolder_id = Database.get1(Database.execute(conn, query="SELECT folder_id FROM vlacs_class_folders_structure WHERE folder_name = 'VLACS Class Folders';"))
 
+            # TODO: create a function that creates the title, given a enrollment. E.g.: Utilities.title(enrollment)
+            classfolder = Folder.create(client, enrollment['course_name'] + " - " + Utilities.clean_name(enrollment['teacher_firstname']) + " " + Utilities.clean_name(enrollment['teacher_lastname']) + " - " + Utilities.course_version(enrollment['course_full_name']) + " - " + enrollment['class_id'], rootclassfolder_id['folder_id'], enrollment['class_id'])
+            # TODO: see above WRT Utilities.title(enrollment)
+            Folder.create(client, Utilities.clean_name(enrollment['student_lastname']) + ", " + Utilities.clean_name(enrollment['student_firstname']) + " - Assignments", classfolder.resource_id.text)
+        count += 1
+    # TODO: do all the cursors need to be closed explicitly as well?
+    Database.close(conn)
+
+# TODO: consider getopt() for make benefit glorious CLI
 if __name__ == "__main__":
     if len(sys.argv) > 2:
         main(sys.argv[1], sys.argv[2])
