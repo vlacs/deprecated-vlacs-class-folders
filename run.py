7e346ed392ae33be8283556c1394ad6aeec6bd5e
Mike George <mikeyg6754@gmail.com>
2013-10-21 11:04:28 -0400

    Test compare_db_with_drive

--- run.py
+++ run.py
@@ -195,7 +195,7 @@ def compare_db_with_drive(client, conn):
             gd_root_folders[resource.title.text] = resource.resource_id.text
 
     # STORE LIST OF CONTENTS (TITLE BY ID) FROM ROOT FOLDER #
-    for resource in client.GetAllResources(uri="/feeds/default/private/full/%s/contents/-/folder" % (gd_root_folders[config.ROOT_CLASS_FOLDER]))
+    for resource in client.GetAllResources(uri="/feeds/default/private/full/%s/contents/-/folder" % gd_root_folders[config.ROOT_CLASS_FOLDER])
         if resource.GetResourceType() == 'folder':
             gd_contents[resource.resource_id.text] = resource.title.text
 
