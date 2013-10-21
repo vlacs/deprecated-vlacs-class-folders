659cc5ae3262ffbb0f6674369b8df873f251aaec
Mike George <mikeyg6754@gmail.com>
2013-10-21 10:28:11 -0400

    Convert Indentation to Spaces

--- Libs/Color.py
+++ Libs/Color.py
@@ -1,19 +1,19 @@
 __author__ = 'mgeorge@vlacs.org (Mike George)'
 
 def red(str):
-	print('\x1b[31m' + str + '\x1b[39m')
+    print('\x1b[31m' + str + '\x1b[39m')
 
 def green(str):
-	print('\x1b[32m' + str + '\x1b[39m')
+    print('\x1b[32m' + str + '\x1b[39m')
 
 def yellow(str):
-	print('\x1b[33m' + str + '\x1b[39m')
+    print('\x1b[33m' + str + '\x1b[39m')
 
 def blue(str):
-	print('\x1b[34m' + str + '\x1b[39m')
+    print('\x1b[34m' + str + '\x1b[39m')
 
 def magenta(str):
-	print('\x1b[35m' + str + '\x1b[39m')
+    print('\x1b[35m' + str + '\x1b[39m')
 
 def cyan(str):
-	print('\x1b[36m' + str + '\x1b[39m')
+    print('\x1b[36m' + str + '\x1b[39m')
--- tests/folder_structure.py
+++ tests/folder_structure.py
@@ -8,19 +8,19 @@ import gdata.docs.client
 import gdata.docs.data
 
 def main():
-	client = Client.create()
+    client = Client.create()
 
-	#folder_count = get_resources('folder:0B7AqvGrb_oO8SDYwem5zcTdrMnM', client)
-	folder_count = get_resources('root', client)
+    #folder_count = get_resources('folder:0B7AqvGrb_oO8SDYwem5zcTdrMnM', client)
+    folder_count = get_resources('root', client)
 
-	print "Done!"
+    print "Done!"
 
 def get_resources(collection_id, client, recursed=False):
-	for resource in client.GetAllResources(uri="/feeds/default/private/full/%s/contents/-/folder" % collection_id, show_root=True):
-		print resource.title.text, '|', resource.resource_id.text, '|', resource.GetResourceType(), '| Parent:', collection_id, '| Recursed: ' + str(recursed)
-		if resource.GetResourceType() == 'folder':
-			if not recursed:
-				count = get_resources(resource.resource_id.text, client, True)
+    for resource in client.GetAllResources(uri="/feeds/default/private/full/%s/contents/-/folder" % collection_id, show_root=True):
+        print resource.title.text, '|', resource.resource_id.text, '|', resource.GetResourceType(), '| Parent:', collection_id, '| Recursed: ' + str(recursed)
+        if resource.GetResourceType() == 'folder':
+            if not recursed:
+                count = get_resources(resource.resource_id.text, client, True)
 
 if __name__ == "__main__":
-	main()
+    main()
