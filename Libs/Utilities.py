7c04231713f6455d7295133eb777c98cb04cdc6f
Mike George <mikeyg6754@gmail.com>
2013-10-27 12:36:58 -0400

    debug not_synced

--- Libs/Utilities.py
+++ Libs/Utilities.py
@@ -50,14 +50,9 @@ def fix_nulls(dict):
 
 def not_synced(enrollment, database_contents):
     for entry in database_contents:
-        print "DEBUG: ", gen_title(enrollment, "s"), "|", entry['folder_name'], "|", enrollment['class_id'], "|", entry['class_id'] 
-        if (gen_title(enrollment, "s") == entry['folder_name']):
-            if(int(enrollment['class_id']) == int(entry['class_id'])):
-                rVal = False
-            else:
-                print "DEBUG: ", enrollment['class_id'], "!=", entry['class_id']
-                rVal = True
+        if (gen_title(enrollment, "s") == entry['folder_name'] and 
+            int(enrollment['class_id']) == int(entry['class_id'])):
+            rVal = False
         else:
             rVal = True
-    print "DEBUG: ", rVal
     return rVal
