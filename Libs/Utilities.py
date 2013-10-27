8cec4b0df45788a4ce67406c309da1e6d291985c
Mike George <mikeyg6754@gmail.com>
2013-10-27 12:31:22 -0400

    debug not_synced

--- Libs/Utilities.py
+++ Libs/Utilities.py
@@ -52,7 +52,10 @@ def not_synced(enrollment, database_contents):
     for entry in database_contents:
         print "DEBUG: ", gen_title(enrollment, "s"), "|", entry['folder_name'], "|", enrollment['class_id'], "|", entry['class_id'] 
         if (gen_title(enrollment, "s") == entry['folder_name']):
-            rVal = False
+            if(enrollment['class_id'] == entry['class_id']):
+                rVal = False
+            else:
+                rVal = True
         else:
             rVal = True
     print "DEBUG: ", rVal
