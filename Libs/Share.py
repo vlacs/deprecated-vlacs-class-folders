e289a29ac43d680d96e360a5f20bd47e0eed123d
Mike George <mikeyg6754@gmail.com>
2013-11-21 10:04:57 -0500

    Check for max_level

--- Libs/Share.py
+++ Libs/Share.py
@@ -26,16 +26,20 @@ def create_share_structure(client, conn, folder_entry):
 	enrollment = Database.get(Database.execute(conn, Database.enrollment_query_string(where="class_id = '" + folder_entry['class_id'] + "' AND student_id = '" + folder_entry['student_id'] + "'")))
 	parent_res_id = ""
 	directory_folders = None
+	max_level = 0
 
 	structures = retrieve_share_structures()
 
 	for name, structure in structures.iteritems():
 		for template, level in structure.iteritems():
+			if (level > max_level):
+				max_level = level
+		for template, level in structure.iteritems():
 			folder = ShareTemplate.get(client, conn, template, enrollment)
 
 			if level == 0:
 				directory_folders = Folder.list_sub_folders(client, folder['folder_id'])
-				parent_res_id = folder['folder_id']
+				parent_res_id = folder['folder_id']	
 			else:				
 				directory_folders = Folder.list_sub_folders(client, parent_res_id)
 
@@ -44,12 +48,14 @@ def create_share_structure(client, conn, folder_entry):
 					#If the folder is already there, store the resource_id and move on
 					if folder['folder_name'] in directory_folders:
 						print "DEBUG: Folder exists, ", directory_folders[folder['folder_name']]
-						parent_res_id = directory_folders[folder['folder_name']]
+						if level != max_level:
+							parent_res_id = directory_folders[folder['folder_name']]
 					#If the folder is not there, create it, store the id, and move on
 					else:
 						print "DEBUG: Creating new folder"
 						new_folder = create_folder(client, folder['folder_name'], parent_res_id)
-						parent_res_id = new_folder.resource_id.text
+						if level != max_level:
+							parent_res_id = new_folder.resource_id.text
 				elif 'folder_id' in folder:
 					print "DEBUG: Copying assignment folder"
 					Folder.copy(client, folder['folder_id'], parent_res_id)
