3c55e29b6f58eea9373c982a7a6346f64de5301b
Mike George <mikeyg6754@gmail.com>
2013-11-21 16:16:13 -0500

    Update ShareStructure.py to use a data structure instead of ifelse

--- Libs/ShareTemplate.py
+++ Libs/ShareTemplate.py
@@ -19,6 +19,8 @@ from Libs import Utilities
 def get(client, conn, template, enrollment=None):
 	parsed_template = {'folder_name': "", 'role': {'teacher': "", 'student': ""}}
 
+	# These template variables rely on external systems, we shouldn't generate them
+	# each time the function is called.
 	if template == "{{TEACHER_ROOT}}":
 		root_folders = Folder.list_sub_folders(client, "root")
 		parsed_template['folder_id'] = root_folders[config.TEACHER_SHARE_FOLDER]
@@ -31,36 +33,6 @@ def get(client, conn, template, enrollment=None):
 		parsed_template['role']['teacher'] = 'reader'
 		parsed_template['role']['student'] = 'reader'
 		parsed_template['isassignment'] = False
-	elif template == "{{STUDENTS}}":
-		parsed_template['folder_name'] = "Students"
-		parsed_template['role']['teacher'] = 'reader'
-		parsed_template['role']['student'] = 'reader'
-		parsed_template['isassignment'] = False
-	elif template == "{{COURSES}}":
-		parsed_template['folder_name'] = "Courses"
-		parsed_template['role']['teacher'] = 'reader'
-		parsed_template['role']['student'] = 'reader'
-		parsed_template['isassignment'] = False
-	elif template == "{{STUDENT_NAME}}":
-		parsed_template['folder_name'] = "%s, %s" % (enrollment['student_lastname'], enrollment['student_firstname'])
-		parsed_template['role']['teacher'] = 'reader'
-		parsed_template['role']['student'] = 'reader'
-		parsed_template['isassignment'] = False
-	elif template == "{{COURSE_NAME}}":
-		parsed_template['folder_name'] = "%s" % (enrollment['course_name'])
-		parsed_template['role']['teacher'] = 'reader'
-		parsed_template['role']['student'] = 'reader'
-		parsed_template['isassignment'] = False
-	elif template == "{{CLASSROOM}}":
-		parsed_template['folder_name'] = "%s - %s" % (enrollment['class_id'], Utilities.course_version(enrollment['course_full_name']))
-		parsed_template['role']['teacher'] = 'reader'
-		parsed_template['role']['student'] = 'reader'
-		parsed_template['isassignment'] = False
-	elif template == "{{CLASS_FILES}}":
-		parsed_template['folder_name'] = "Class Files"
-		parsed_template['role']['teacher'] = 'writer'
-		parsed_template['role']['student'] = 'reader'
-		parsed_template['isassignment'] = False
 	elif template == "{{STUDENT_ASSIGNMENTS}}":
 		folder_id = Database.get(Database.execute(conn, Database.structure_get_folder_id_string(Utilities.gen_title(enrollment, "s"), enrollment['class_id'], enrollment['student_id'])))
 		folder_id = folder_id['folder_id']
@@ -68,5 +40,57 @@ def get(client, conn, template, enrollment=None):
 		parsed_template['role']['teacher'] = 'writer'
 		parsed_template['role']['student'] = 'writer'
 		parsed_template['isassignment'] = True
+	else:
+		template_variables = {
+			"{{CLASS_FILES}}" : {
+					 'folder_name' : "Class Files",
+					        'role' : {
+										'teacher' : 'writer',
+										'student' : 'reader',
+									},
+					'isassignment' : False
+			},
+			"{{CLASSROOM}}" : {
+					 'folder_name' : "%s (%s - %s)" % (enrollment['course_name'], enrollment['class_id'], Utilities.course_version(enrollment['course_full_name'])),
+					        'role' : {
+										'teacher' : 'reader',
+										'student' : 'reader',
+									},
+					'isassignment' : False
+			},
+			"{{COURSE_NAME}}" : {
+					 'folder_name' : enrollment['course_name'],
+					        'role' : {
+										'teacher' : 'reader',
+										'student' : 'reader',
+									},
+					'isassignment' : False
+			},
+			"{{COURSES}}" : {
+					 'folder_name' : "Courses",
+					        'role' : {
+										'teacher' : 'reader',
+										'student' : 'reader',
+									},
+					'isassignment' : False
+			},
+			"{{STUDENT_NAME}}" : {
+					 'folder_name' : "%s, %s" % (enrollment['student_lastname'], enrollment['student_firstname']),
+					        'role' : {
+										'teacher' : 'reader',
+										'student' : 'reader',
+									},
+					'isassignment' : False
+			},
+			"{{STUDENTS}}" : {
+					 'folder_name' : "Students",
+					        'role' : {
+										'teacher' : 'reader',
+										'student' : 'reader',
+									},
+					'isassignment' : False
+			},
+		}
+		parsed_template = template_variables[template]
 
 	return parsed_template
