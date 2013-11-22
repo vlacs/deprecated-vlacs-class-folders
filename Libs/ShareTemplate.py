dd3c9ad196857e03d446ef3fd81e5f516bba4888
Mike George <mikeyg6754@gmail.com>
2013-11-21 21:26:56 -0500

    Update ShareTemplate.py to use data structure insead of a mess of ifs and elses

--- Libs/ShareTemplate.py
+++ Libs/ShareTemplate.py
@@ -6,42 +6,20 @@ from Libs import Folder
 from Libs import Utilities
 
 ## Accepted Template Variables:
-##   {{TEACHER_ROOT}} Root Teacher Share Folder
+##   {{CLASS_FILES}} Teacher writable Student accessable folder for class files
+##	 {{CLASSROOM}} Folder with Classroom ID and Version
+##   {{COURSE_NAME}} Folder with Master Course Name
+##   {{COURSES}} Folder called Courses
+##   {{STUDENT_ASSIGNMENTS}} Students' assignment folder
+##   {{STUDENT_NAME}} Folder with student name (Last Name, First Name)
 ##   {{STUDENT_ROOT}} Root Student Share Folder
 ##   {{STUDENTS}} Folder called Students
-##   {{COURSES}} Folder called Courses
-##   {{STUDENT_NAME}} Folder with student name (Last Name, First Name)
-##   {{COURSE_NAME}} Folder with Master Course Name
-##   {{CLASSROOM}} Folder with Classroom ID and Version
-##   {{CLASS_FILES}} Teacher writable Student accessable folder for class files
-##   {{STUDENT_ASSIGNMENTS}} Students' assignment folder
+##   {{TEACHER_ROOT}} Root Teacher Share Folder
 
 def get(client, conn, template, enrollment=None):
 	parsed_template = {'folder_name': "", 'role': {'teacher': "", 'student': ""}}
 
-	# These template variables rely on external systems, we shouldn't generate them
-	# each time the function is called.
-	if template == "{{TEACHER_ROOT}}":
-		root_folders = Folder.list_sub_folders(client, "root")
-		parsed_template['folder_id'] = root_folders[config.TEACHER_SHARE_FOLDER]
-		parsed_template['role']['teacher'] = 'reader'
-		parsed_template['role']['student'] = 'reader'
-		parsed_template['isassignment'] = False
-	elif template == "{{STUDENT_ROOT}}":
-		root_folders = Folder.list_sub_folders(client, "root")
-		parsed_template['folder_id'] = root_folders[config.STUDENT_SHARE_FOLDER]
-		parsed_template['role']['teacher'] = 'reader'
-		parsed_template['role']['student'] = 'reader'
-		parsed_template['isassignment'] = False
-	elif template == "{{STUDENT_ASSIGNMENTS}}":
-		folder_id = Database.get(Database.execute(conn, Database.structure_get_folder_id_string(Utilities.gen_title(enrollment, "s"), enrollment['class_id'], enrollment['student_id'])))
-		folder_id = folder_id['folder_id']
-		parsed_template['folder_id'] = folder_id
-		parsed_template['role']['teacher'] = 'writer'
-		parsed_template['role']['student'] = 'writer'
-		parsed_template['isassignment'] = True
-	else:
-		template_variables = {
+	template_variables = {
 			"{{CLASS_FILES}}" : {
 					 'folder_name' : "Class Files",
 					        'role' : {
@@ -74,8 +52,24 @@ def get(client, conn, template, enrollment=None):
 									},
 					'isassignment' : False
 			},
+			"{{STUDENT_ASSIGNMENTS}}" : {
+					 'folder_id' : "",
+					        'role' : {
+										'teacher' : 'writer',
+										'student' : 'reader',
+									},
+					'isassignment' : True
+			},
 			"{{STUDENT_NAME}}" : {
 					 'folder_name' : "%s, %s" % (enrollment['student_lastname'], enrollment['student_firstname']),
+					        'role' : {
+										'teacher' : 'reader',
+										'student' : 'reader',
+									},
+					'isassignment' : False
+			},
+			"{{STUDENT_ROOT}}" : {
+					 'folder_id' : "",
 					        'role' : {
 										'teacher' : 'reader',
 										'student' : 'reader',
@@ -90,7 +84,29 @@ def get(client, conn, template, enrollment=None):
 									},
 					'isassignment' : False
 			},
+			"{{TEACHER_ROOT}}" : {
+					 'folder_id' : "",
+					        'role' : {
+										'teacher' : 'reader',
+										'student' : 'reader',
+									},
+					'isassignment' : False
+			},
 		}
-		parsed_template = template_variables[template]
+
+	# These template variables rely on external systems, we shouldn't generate them
+	# each time the function is called.
+	if template == "{{TEACHER_ROOT}}":
+		root_folders = Folder.list_sub_folders(client, "root")
+		template_variables[template]['folder_id'] = root_folders[config.TEACHER_SHARE_FOLDER]
+	elif template == "{{STUDENT_ROOT}}":
+		root_folders = Folder.list_sub_folders(client, "root")
+		template_variables[template]['folder_id'] = root_folders[config.STUDENT_SHARE_FOLDER]
+	elif template == "{{STUDENT_ASSIGNMENTS}}":
+		folder_id = Database.get(Database.execute(conn, Database.structure_get_folder_id_string(Utilities.gen_title(enrollment, "s"), enrollment['class_id'], enrollment['student_id'])))
+		folder_id = folder_id['folder_id']
+		template_variables[template]['folder_id'] = folder_id
+		
+	parsed_template = template_variables[template]
 
 	return parsed_template
