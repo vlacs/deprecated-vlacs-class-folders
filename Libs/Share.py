d12c26aba80f7ba55247d24c4f6b9006bb26281b
Mike George <mikeyg6754@gmail.com>
2013-12-09 10:17:08 -0500

    fix Share.py

--- Libs/Share.py
+++ Libs/Share.py
@@ -46,8 +46,9 @@ def share(client, folder_id, share_with, role):
             print "DEBUG: ACL for %s exists, verifying role." % share_with
             update_acl = acl
     if update_acl:
-        print "DEBUG: Current ACL entry Okay."
-        if not update_acl.role.value == role:
+        if update_acl.role.value == role:
+            print "DEBUG: Current ACL entry Okay."
+        else:
             print "DEBUG: ACL Scope: %s ACL Role: %s" % (update_acl.scope.value, update_acl.role.value)
             update_acl.role.value = role
             update_acl.etag = None
