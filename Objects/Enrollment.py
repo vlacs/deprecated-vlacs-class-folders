69cf2a7b080c6060980ba6850487f384d673dcdd
Mike George <mikeyg6754@gmail.com>
2013-12-30 13:51:36 -0500

    Fix create_list_from_dict function

--- Objects/Enrollment.py
+++ Objects/Enrollment.py
@@ -37,25 +37,26 @@ class Enrollment:
 			                   db_result['teacher_firstname'],
 			                   db_result['teacher_lastname'],
 			                   db_result['teacher_email'])
-		
-	def create_list_from_dict(self, db_result_dict):
+
+	def create_list_from_dict(db_result_dict):
 		return_list = []
 
 		for enrollment in db_result_dict:
-			self.master_id = enrollment['master_id']
-			self.course = Course(enrollment['course_id'],
+			tmp_enr = Enrollment()
+			tmp_enr.master_id = enrollment['master_id']
+			tmp_enr.course = Course(enrollment['course_id'],
 				                 enrollment['course_name'],
 				                 Utilities.course_version(enrollment['course_full_name']))
 
-			self.student = Student(enrollment['student_id'],
+			tmp_enr.student = Student(enrollment['student_id'],
 								   enrollment['student_firstname'],
 								   enrollment['student_lastname'],
 								   enrollment['student_email'])
 
-			self.teacher = Teacher(enrollment['teacher_id'],
+			tmp_enr.teacher = Teacher(enrollment['teacher_id'],
 				                   enrollment['teacher_firstname'],
 				                   enrollment['teacher_lastname'],
 				                   enrollment['teacher_email'])
-			return_list.append(self)
+			return_list.append(tmp_enr)
 
 		return return_list
