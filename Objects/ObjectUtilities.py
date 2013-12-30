cfc200858250ded7ed31091a5d9795d495854bd7
Mike George <mikeyg6754@gmail.com>
2013-12-30 16:14:39 -0500

    Use Enrollment.create() function

--- Objects/ObjectUtilities.py
+++ Objects/ObjectUtilities.py
@@ -2,33 +2,16 @@
 
 __author__ = 'mgeorge@vlacs.org (Mike George)'
 
-from Course import Course
 from Enrollment import Enrollment
-from Student import Student
-from Teacher import Teacher
 
 from Libs import Utilities
 
 def enrollment_list_from_dict(db_result_dict):
-	return_list = []
+	result_list = []
 
 	for enrollment in db_result_dict:
 		tmp_enr = Enrollment()
-
-		tmp_enr.master_id = enrollment['master_id']
-		tmp_enr.course = Course(enrollment['course_id'],
-			                 enrollment['course_name'],
-			                 Utilities.course_version(enrollment['course_full_name']))
-
-		tmp_enr.student = Student(enrollment['student_id'],
-							   enrollment['student_firstname'],
-							   enrollment['student_lastname'],
-							   enrollment['student_email'])
-
-		tmp_enr.teacher = Teacher(enrollment['teacher_id'],
-			                   enrollment['teacher_firstname'],
-			                   enrollment['teacher_lastname'],
-			                   enrollment['teacher_email'])
+		tmp_enr.create(enrollment)
 		return_list.append(tmp_enr)
 
-	return return_list
+	return result_list
